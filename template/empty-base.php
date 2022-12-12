fc5e8b0e5b93f9a24775b0519579773e59ef3fd0
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-12-12 15:37:18 +0100

    Add style sheet for login

--- template/empty-base.php
+++ template/empty-base.php
@@ -3,7 +3,7 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?php echo $templateParams["title"]; ?></title>
-    <link rel="stylesheet" type="text/css" href="./css/style.css" />
+    <link rel="stylesheet" type="text/css" href="./css/login.css" />
     
     <!--Pico.css-->
     <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css">
