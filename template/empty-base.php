32ffe66adf0df4244e63f9fb7322c9a6cc808189
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-12-12 15:27:06 +0100

    Create a base template for login and sign up page

---
+++ template/empty-base.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title><?php echo $templateParams["title"]; ?></title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css" />
+    
+    <!--Pico.css-->
+    <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css">
+</head>
+<body>
+    <header>
+        <h1>Nome sito</h1>
+    </header>
+    <main class="container">
+    </main>
+    <?php
+    if(isset($templateParams["js"])):
+        foreach($templateParams["js"] as $script):
+    ?>
+        <script src="<?php echo $script; ?>"></script>
+    <?php
+        endforeach;
+    endif;
+    ?>
+</body>
+</html>
