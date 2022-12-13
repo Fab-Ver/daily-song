779a867b77d553297b0261bfb0d827eb23066d72
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-12-13 12:28:15 +0100

    Add function to check if email and user is in database

--- db/database.php
+++ db/database.php
@@ -8,5 +8,25 @@ class DatabaseHelper{
             die("Connection failed: " . $this->db->connect_error);
         }        
     }
+
+    public function getUser($email){
+        $query = "SELECT username, email, passwordHash FROM profile WHERE email = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('s',$email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function checkUsername($username){
+        $query = "SELECT username FROM profile WHERE username = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('s',$username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
 }
 ?>
