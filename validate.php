a5bf994b2dc429f2b84f159d3767534b7e1f8607
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-12-14 12:12:04 +0100

    Check form data and insert into database

--- validate.php
+++ validate.php
@@ -11,6 +11,27 @@ if(isset($_POST["checkEmail"])){
     if(count($dbh->checkUsername($_POST["checkUsername"])) > 0){
         $result["errorUsername"] = true;
     }
+} else if(isset($_POST["email"]) &&
+          isset($_POST["first_name"]) &&
+          isset($_POST["last_name"]) &&
+          isset($_POST["birth_date"]) && 
+          isset($_POST["telephone"]) &&
+          isset($_POST["username"]) &&
+          isset($_POST["password"]) &&
+          isset($_POST["profile_picture"]) &&
+          isset($_POST["notification"])){
+        $hash = password_hash($_POST["password"],PASSWORD_DEFAULT);
+        if($dbh->insertUser($_POST["email"],$_POST["first_name"],$_POST["last_name"],$_POST["birth_date"],$_POST["telephone"],$_POST["username"],$hash,$_POST["profile_picture"],$_POST["notification"])){
+            $result["validateError"] = false;
+            session_regenerate_id();
+            $_SESSION['loggedIn'] = true;
+            $_SESSION['email'] = $_POST["email"];
+            $_SESSION['id'] = $_POST["username"];
+        } else {
+            $result["validateError"] = true;
+        }         
+} else {
+    $result["validateError"] = true;
 }
 
 header('Content-Type: application/json');
