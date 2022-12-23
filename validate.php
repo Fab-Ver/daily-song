eee75bd842150f5a4386c671efff991f636ba382
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-12-24 00:14:46 +0100

    check if user is logged, change result structure

--- validate.php
+++ validate.php
@@ -1,40 +1,37 @@
 <?php
 require_once 'bootstrap.php';
+secure_session_start();
 
-if(isset($_POST["checkEmail"])){
-    $result["errorEmail"] = false;
-    if(count($dbh->getUser($_POST["checkEmail"])) > 0){
-        $result["errorEmail"] = true;
+$result['loggedIn'] = false;
+$result["errorEmail"] = false;
+$result["errorUsername"] = false;
+$result["validateError"] = false;
+
+if(!isUserLoggedIn()){
+    if(isset($_POST["checkEmail"])){
+        if(count($dbh->getUser($_POST["checkEmail"])) > 0){
+            $result["errorEmail"] = true;
+        }
+    } else if (isset($_POST["checkUsername"])){
+        if(count($dbh->checkUsername($_POST["checkUsername"])) > 0){
+            $result["errorUsername"] = true;
+        }
+    } else if(isset($_POST["email"],$_POST["first_name"],$_POST["last_name"],$_POST["birth_date"],$_POST["telephone"],$_POST["username"],$_POST["password"],$_POST["profile_picture"],$_POST["notification"],$_POST["favoriteGenres"])){
+            $hash = password_hash($_POST["password"],PASSWORD_DEFAULT);
+            if($dbh->insertUser($_POST["email"],$_POST["first_name"],$_POST["last_name"],$_POST["birth_date"],$_POST["telephone"],$_POST["username"],$hash,$_POST["profile_picture"])){
+                $dbh->insertSettings($_POST["username"],$_POST["notification"]);
+                $genresIDs = json_decode($_POST["favoriteGenres"]);
+                $dbh->insertFavoriteGenres($_POST["username"],$genresIDs);
+                registerLoggedUser($_POST["username"],$_POST["email"]);
+                $result['loggedIn'] = true;
+            } else {
+                $result["validateError"] = true;
+            }         
+    } else {
+        $result["validateError"] = true;
     }
-} else if (isset($_POST["checkUsername"])){
-    $result["errorUsername"] = false;
-    if(count($dbh->checkUsername($_POST["checkUsername"])) > 0){
-        $result["errorUsername"] = true;
-    }
-} else if(isset($_POST["email"]) &&
-          isset($_POST["first_name"]) &&
-          isset($_POST["last_name"]) &&
-          isset($_POST["birth_date"]) && 
-          isset($_POST["telephone"]) &&
-          isset($_POST["username"]) &&
-          isset($_POST["password"]) &&
-          isset($_POST["profile_picture"]) &&
-          isset($_POST["notification"]) &&
-          isset($_POST["favoriteGenres"])){
-        $hash = password_hash($_POST["password"],PASSWORD_DEFAULT);
-        if($dbh->insertUser($_POST["email"],$_POST["first_name"],$_POST["last_name"],$_POST["birth_date"],$_POST["telephone"],$_POST["username"],$hash,$_POST["profile_picture"])){
-            $dbh->insertSettings($_POST["username"],$_POST["notification"]);
-            $genresIDs = json_decode($_POST["favoriteGenres"]);
-            $dbh->insertFavoriteGenres($_POST["username"],$genresIDs);
-            $result["validateError"] = false;
-            $_SESSION['loggedIn'] = true;
-            $_SESSION['email'] = $_POST["email"];
-            $_SESSION['username'] = $_POST["username"];
-        } else {
-            $result["validateError"] = true;
-        }         
 } else {
-    $result["validateError"] = true;
+    $result['loggedIn'] = true;
 }
 
 header('Content-Type: application/json');
