d2bc03f667b76e3b70696e314944c58f5e44376c
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-12-24 00:23:06 +0100

    Add redirect if user is logged in

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
 require_once 'bootstrap.php';
+secure_session_start();
 
-$templateParams["title"] = "Nome sito - Login";
-$templateParams["js"] = array("https://unpkg.com/axios/dist/axios.min.js","utils/functions.js","js/index.js");
-
-require 'template/empty-base.php';
+if(!isUserLoggedIn()){
+    $templateParams["title"] = "Nome sito - Login";
+    $templateParams["js"] = array("https://unpkg.com/axios/dist/axios.min.js","utils/functions.js","js/index.js");
+    require 'template/empty-base.php';
+} else {
+    header('Location: homepage.php');
+}
 
 ?>
