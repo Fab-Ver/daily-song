ced66cc5e8e7cf4b4b009011865b6fd17d45c1f2
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-12-24 00:33:37 +0100

    Check if user is logged in

--- new_post.php
+++ new_post.php
@@ -1,8 +1,14 @@
 <?php
 require_once 'bootstrap.php';
+secure_session_start();
 
-$templateParams["title"] = "Nome sito - New Post";
-$templateParams["js"] = array("js/config.js","https://unpkg.com/axios/dist/axios.min.js","utils/functions.js","js/new_post.js");
+if(isUserLoggedIn()){
+    $templateParams["title"] = "Nome sito - New Post";
+    $templateParams["js"] = array("js/config.js","https://unpkg.com/axios/dist/axios.min.js","utils/functions.js","js/new_post.js");
 
-require 'template/base.php';
+    require 'template/base.php';
+} else {
+    header('Location: index.php');
+}
+
 ?>
