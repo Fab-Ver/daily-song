aec616c919c06ca90226cc06c67d99eb75f9275e
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-12-24 00:10:07 +0100

    Add redirect if use is logged

--- signup.php
+++ signup.php
@@ -1,9 +1,13 @@
 <?php
 require_once 'bootstrap.php';
+secure_session_start();
 
-$templateParams["title"] = "Nome sito - Sign Up";
-$templateParams["js"] = array("https://unpkg.com/axios/dist/axios.min.js","utils/functions.js","js/signup.js");
+if(!isUserLoggedIn()){
+    $templateParams["title"] = "Nome sito - Sign Up";
+    $templateParams["js"] = array("https://unpkg.com/axios/dist/axios.min.js","utils/functions.js","js/signup.js");
 
-require 'template/empty-base.php';
-
+    require 'template/empty-base.php';
+} else {
+    header('Location: homepage.php');
+}
 ?>
