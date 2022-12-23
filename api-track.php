e85a82a8f9a3ea593ac3ef448d9675c4ebf16f99
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-12-24 00:43:32 +0100

    Check if user is logged in, change isset

--- api-track.php
+++ api-track.php
@@ -1,18 +1,17 @@
 <?php
 require_once 'bootstrap.php';
-
+secure_session_start();
 $result["checkTrackID"] = false;
 
-if(isset($_POST["checkTrackID"])){
-    $result["checkTrackID"] = $dbh->checkTrack($_POST["checkTrackID"]);
-} else if (isset($_POST["trackID"]) && 
-           isset($_POST["urlSpotify"]) &&
-           isset($_POST["urlImage"]) &&
-           isset($_POST["urlPreview"]) &&
-           isset($_POST["title"]) &&
-           isset($_POST["artist"]) &&
-           isset($_POST["albumName"])){
+if(isUserLoggedIn()){
+    $result['loggedIn'] = true;
+    if(isset($_POST["checkTrackID"])){
+        $result["checkTrackID"] = $dbh->checkTrack($_POST["checkTrackID"]);
+    } else if (isset($_POST["trackID"],$_POST["urlSpotify"],$_POST["urlImage"],$_POST["urlPreview"],$_POST["title"],$_POST["artist"],$_POST["albumName"])){
         $dbh->insertTrack($_POST["trackID"], $_POST["urlSpotify"], $_POST["urlImage"], $_POST["urlPreview"], $_POST["title"], $_POST["artist"], $_POST["albumName"]);
+    }
+} else {
+    $result['loggedIn'] = false;
 }
 
 header('Content-Type: application/json');
