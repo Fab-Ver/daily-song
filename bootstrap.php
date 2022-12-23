2b97b6e72f1dbb94e37feaa05aa01cbea56bbaf0
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-12-24 00:26:53 +0100

    Remove secure session start

--- bootstrap.php
+++ bootstrap.php
@@ -2,6 +2,5 @@
 define("UPLOAD_DIR", "./res/");
 require_once("db/database.php");
 require_once("utils/functions.php");
-secure_session_start();
 $dbh = new DatabaseHelper("localhost", "secure_user", "p5N3RHN9fWE5QRvxxuPcpJXZ", "progetto_web", 3306);
 ?>
