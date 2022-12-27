373a95a4b7d4eb1721493e8bf737c3bee85fcc36
LucaBighini <bighini.luca@gmail.com>
2022-12-27 14:38:47 +0100

    rename homepage

--- homepage.php
+++ homepage.php
@@ -4,7 +4,7 @@ secure_session_start();
 
 if(isUserLoggedIn()){
     $templateParams["title"] = "Nome sito - Home";
-    $templateParams["js"] = array("js/homepage.js");
+    $templateParams["js"] = array("js/config.js","js/home.js");
     $genres = $dbh->getGenres();
 
     require 'template/base.php';
