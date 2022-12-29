6b21c77e226827b9957a791dcd2e670dbd978e5e
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-12-29 23:06:14 +0100

    Add server-side validation, change error structure and insert login code inside login function in utils/functions.php

--- authenticate.php
+++ authenticate.php
@@ -2,35 +2,54 @@
 require_once 'bootstrap.php';
 secure_session_start();
 $result['loggedIn'] = false;
-$result["errorPassword"] = false;
-$result["checkBrute"] = false;
 
 if(!isUserLoggedIn()){
+    $result['errorMsg'] = "";
+    $result['elemID'] = "";
     if(isset($_POST["checkEmail"])){
-        $result["errorEmail"] = false;
-        if(count($dbh->getUser($_POST["checkEmail"])) == 0){
-            $result["errorEmail"] = true;
-        }
-    } else if (isset($_POST["email"]) && isset($_POST["password"])){
-        $email = $_POST["email"];
-        $password = $_POST["password"];
-        $user = $dbh->getUser($email);
-        $hash = $user[0]["passwordHash"];
-        if($dbh->checkBrute($user[0]["username"])){
-            $result["checkBrute"] = true;
-        } else {
-            if(password_verify($password,$hash)){
-                registerLoggedUser($user[0]['username'],$user[0]['email']);
-                $result["loggedIn"] = true;
-            } else {
-                $dbh->insertLoginAttempts($user[0]["username"]);
-                $result["errorPassword"] = true;
+        $email = $_POST['checkEmail'];
+        if(validateEmail($email)){
+            if(count($dbh->findUsernameByEmail($email)) == 0){
+                $result['errorMsg'] = NO_USER;
             }
+        } else {
+            $result['errorMsg'] = INVALID_EMAIL;
         }
+    } else if (isset($_POST['email'], $_POST['password'], $_POST['remember_me'])){
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $remember_me = filter_var($_POST['remember_me'], FILTER_VALIDATE_BOOLEAN);
+
+        if(validateEmail($email)){
+            $user = $dbh->findUsernameByEmail($email);
+            if(count($user) != 0){
+                $username = $user[0]['username'];
+                if($dbh->isUserActive($username)){
+                    if(login($username,$password,$remember_me)){
+                        $result['loggedIn'] = true;
+                    } else {
+                        $result['errorMsg'] = WRONG_PASSWORD;
+                        $result['elemID'] = 'login_password';
+                    }
+                } else {
+                    /**
+                    * Aggiungere mail all'utente disabilitato
+                    */
+                    $result['errorMsg'] = DISABLED_USER;
+                }
+            } else {
+                $result['errorMsg'] = NO_USER;
+                $result['elemID'] = 'login_email';
+            }
+        } else {
+            $result['errorMsg'] = INVALID_EMAIL;
+            $result['elemID'] = 'login_email';
+        } 
     }
 } else {
-    $result["loggedIn"] = true;
+    header('Location: homepage.php');
 }
 
 header('Content-Type: application/json');
 echo json_encode($result);
+?>
