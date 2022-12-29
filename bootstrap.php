be10ee91cdb1b653b38255f9a3590320e9993a5e
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-12-29 23:07:56 +0100

    Include error messages

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,6 @@
 <?php
 define("UPLOAD_DIR", "./res/");
+require_once('error_msg.php');
 require_once("db/database.php");
 require_once("utils/functions.php");
 $dbh = new DatabaseHelper("localhost", "secure_user", "p5N3RHN9fWE5QRvxxuPcpJXZ", "progetto_web", 3306);
