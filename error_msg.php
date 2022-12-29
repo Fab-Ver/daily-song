b710e5150d34c01f0c48d1b643f56afdfd00c552
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-12-30 00:27:48 +0100

    Add error messages for mail

--- error_msg.php
+++ error_msg.php
@@ -3,4 +3,6 @@
     define('DISABLED_USER','Too many failed login attempts, your account has been disabled, please try later.');
     define('WRONG_PASSWORD','Wrong password');
     define('INVALID_EMAIL','Invalid email format expected example@domain.com');
+    define('UNDEFINED','An undefined error occurred while trying to process your data, try later.');
+    define('MAIL_NOT_SENT','Message could not be sent, try later');
 ?>
