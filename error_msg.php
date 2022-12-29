eced95bf99db20ed3f3cde7d20e811b0f331ed55
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-12-29 23:06:57 +0100

    File containing definition of some error messages

---
+++ error_msg.php
@@ -0,0 +1,6 @@
+<?php
+    define('NO_USER','No user matches the entered email.');
+    define('DISABLED_USER','Too many failed login attempts, your account has been disabled, please try later.');
+    define('WRONG_PASSWORD','Wrong password');
+    define('INVALID_EMAIL','Invalid email format expected example@domain.com');
+?>
