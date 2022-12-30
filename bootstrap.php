da00dd4003449b28cb43a0a2eadaadbf36e6d66e
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-12-30 21:22:47 +0100

    Add require input.php

--- bootstrap.php
+++ bootstrap.php
@@ -3,5 +3,6 @@ define("UPLOAD_DIR", "./res/");
 require_once('error_msg.php');
 require_once("db/database.php");
 require_once("utils/functions.php");
+require_once('utils/input.php');
 $dbh = new DatabaseHelper("localhost", "secure_user", "p5N3RHN9fWE5QRvxxuPcpJXZ", "progetto_web", 3306);
 ?>
