699c45074204d31c510cb2e6902057884dcfbeda
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-12-30 21:24:24 +0100

    Add login_string to session variables, change isUserLoggedIn

--- utils/functions.php
+++ utils/functions.php
@@ -9,7 +9,7 @@ function isActive($pagename){
 
 function secure_session_start(){
     $session_name = 'secure_session_id';
-    $secure = true;
+    $secure = false; /** Set to true to use HTTPS protocol */
     $http_only = true;
     ini_set('session.use_only_cookies',1);
     $cookieParams = session_get_cookie_params();
@@ -21,8 +21,21 @@ function secure_session_start(){
 
 function isUserLoggedIn() : bool{
     global $dbh;
-    if(isset($_SESSION['username'])){
-        return true;
+    if(isset($_SESSION['username'], $_SESSION['email'], $_SESSION['login_string'])){
+        $username = Input::filter_string($_SESSION['username']);
+        $login_string = $_SESSION['login_string'];
+        $user_browser = $_SERVER['HTTP_USER_AGENT'];
+        $user = $dbh->findUserByUsername($username);
+        if(count($user) != 0){
+            $passwordHash = $user[0]['passwordHash'];
+            if(password_verify($passwordHash.$user_browser,$login_string)){
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
     }
 
     $token = filter_input(INPUT_COOKIE, 'remember_me', FILTER_UNSAFE_RAW);
@@ -31,16 +44,18 @@ function isUserLoggedIn() : bool{
         $user = $dbh->findUserByToken($token);
         if($user){
             $user_data = $dbh->findUserByUsername($user['username']);
-            return registerLoggedUser($user_data['username'],$user_data['email']);
+            return registerLoggedUser($user_data['username'],$user_data['email'],$user_data['passwordHash']);
         }
     }
     return false;
 }
 
-function registerLoggedUser($username,$email) : bool{
+function registerLoggedUser(string $username, string $email, string $passwordHash) : bool{
     if(session_regenerate_id()){
+        $user_browser = $_SERVER['HTTP_USER_AGENT'];
         $_SESSION['username'] = $username;
         $_SESSION['email'] = $email;
+        $_SESSION['login_string'] = password_hash($passwordHash.$user_browser,PASSWORD_DEFAULT);
         return true;
     }
     return false;
@@ -103,7 +118,10 @@ function canFollow($user1, $user2follower){
     return true;
 }
 
-function hours_date_diff($dateString1,$dateString2){
+/**
+ * Compute the difference in hours between to dates. 
+ */
+function hours_date_diff(string $dateString1, string $dateString2) : int{
     $date1 = date_create($dateString1);
     $date2 = date_create($dateString2);
     $diff=date_diff($date1,$date2);
@@ -119,7 +137,7 @@ function login(string $username, string $password, bool $remember) : bool {
     global $dbh;
     $user = $dbh->findUserByUsername($username);
     if(count($user) != 0 && password_verify($password,$user[0]['passwordHash'])){
-        registerLoggedUser($user[0]['username'],$user[0]['email']);
+        registerLoggedUser($user[0]['username'],$user[0]['email'],$user[0]['passwordHash']);
         if($remember){
             remember_me($user[0]['username']);
         }
@@ -150,13 +168,6 @@ function remember_me(string $username, int $day = 30){
     if($dbh->insertUserToken($username,$selector,$hash_validator,$expiry)){
         setcookie('remember_me', $token, $expired_seconds);
     }
-}
-
-/**
- * Check if email is valid and not empty.
- */
-function validateEmail(string $email) : bool {
-    return !empty($email) && filter_var($email,FILTER_VALIDATE_EMAIL);
 }
 
 ?>
