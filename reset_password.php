9d618f1544e82c8b481fae0e59f7538bcce17ab4
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-12-30 21:31:29 +0100

    Add server-side validation and change error messages

--- reset_password.php
+++ reset_password.php
@@ -4,62 +4,70 @@ secure_session_start();
 
 $templateParams["title"] = "Nome sito - Reset Password";
 
-if(isset($_POST["password"])){
-    $result["tokenError"] = false;
-    $result["errorReset"] = false;
-    $request = $dbh->getResetRequest($_SESSION["token"]);
-    if(count($request)!=0){
-        if(hours_date_diff(date("Y-m-d H:i:s"),$request[0]["expDate"]) < 24){
-            $email = $request[0]["email"];
-            $password = password_hash($_POST["password"],PASSWORD_DEFAULT);
-            $res = $dbh->resetPassword($email,$password);
-            if($res){
-                $dbh->removeToken($_SESSION["token"]);
-                unset($_SESSION["token"]);
-            } else {
-                $result["errorReset"] = true;
-            }
-        } else {
-            $result["tokenError"] = true;
-        }
-    } else {
-        $result["tokenError"] = true;
-    }
-    header('Content-Type: application/json');
-    echo json_encode($result);
-} else {
-    if(!isUserLoggedIn()){
-        if(isset($_GET["token"])){
-            $request = $dbh->getResetRequest($_GET["token"]);
-            if(count($request)!=0){
-                if(hours_date_diff(date("Y-m-d H:i:s"),$request[0]["expDate"]) < 24){
-                    $_SESSION["token"] = $_GET["token"];
-                    $templateParams["js"] = array("https://unpkg.com/axios/dist/axios.min.js","utils/functions.js","js/reset_password.js");
+if(!isUserLoggedIn()){
+    if(isset($_GET['token'])){
+        $token = $_GET['token'];
+        if(Input::is_hex($token)){
+            $request = $dbh->getResetRequest($token);
+            if(count($request) != 0){
+                if(hours_date_diff(date("Y-m-d H:i:s"),$request[0]['expDate']) < 24){
+                    $_SESSION['token'] = $_GET['token'];
+                    $templateParams['js'] = array("https://unpkg.com/axios/dist/axios.min.js","utils/functions.js","js/reset_password.js");
                     require 'template/empty-base.php';
                 } else {
-                    $templateParams["error"] = '
-                    <article>
-                        <h2>Expired Link</h2>
-                        <p>The link is expired. You are trying to use an expired link, which is valid for only 24 hours (1 days after request).</p>
-                        <p><a href="forgot_password.php">Click here</a> to reset password.</p>
-                    </article>';
+                    $templateParams['error'] = EXPIRED_LINK;
                     require 'template/empty-base.php';
                 }
             } else {
-                $templateParams["error"] = '
-                <article>
-                    <h2>Invalid Link</h2>
-                    <p>The link is invalid/expired. Either you did not copy the correct link from the email, or you have already used the token (in this case it is has been disabled).</p>
-                    <p><a href="forgot_password.php">Click here</a> to reset password.</p>
-                </article>';
+                $templateParams['error'] = INVALID_LINK;
                 require 'template/empty-base.php';
             }
-            
         } else {
-            header('Location: index.php');
+            $templateParams['error'] = INVALID_LINK;
+            require 'template/empty-base.php';
         }
+    } else if (isset($_POST['password'], $_POST['confirmPassword'])){
+        $result['errorMsg'] = "";
+        $password = Input::filter_string($_POST['password']);
+        $confirmPassword = Input::filter_string($_POST['confirmPassword']);
+        [$secure,$errorPassword] = Input::is_secure_password($password);
+        if($secure){
+            if(strcmp($password,$confirmPassword) === 0){
+                if(isset($_SESSION['token']) && Input::is_hex($_SESSION['token'])){
+                    $token = $_SESSION['token'];
+                    $request = $dbh->getResetRequest($token);
+                    if(count($request)!=0){
+                        if(hours_date_diff(date("Y-m-d H:i:s"),$request[0]['expDate']) < 24){
+                            $email = $request[0]['email'];
+                            $hash_password = password_hash($password,PASSWORD_DEFAULT);
+                            if($dbh->resetPassword($email,$hash_password)){
+                                $dbh->removeTokens($email);
+                                unset($_SESSION['token']);
+                            } else {
+                                $result['errorMsg'] = UNDEFINED;
+                            }
+                        } else {
+                            $result['errorMsg'] = EXPIRED_TOKEN;
+                        }
+                    } else {
+                        $result['errorMsg'] = INVALID_TOKEN;
+                    }
+                } else {
+                    $result['errorMsg'] = INVALID_TOKEN;
+                }
+            } else {
+                $result['errorMsg'] = PASSWORD_MISMATCH;
+            }
+        } else {
+            $result['errorMsg'] = $errorPassword;
+        }
+        header('Content-Type: application/json');
+        echo json_encode($result);
     } else {
-        header('Location: homepage.php');
+        $templateParams["error"] = INVALID_LINK;
+        require 'template/empty-base.php';
     }
+} else {
+    header('Location: homepage.php');
 }
 ?>
