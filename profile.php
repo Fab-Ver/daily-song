0124c62778e74555adbcd0fde7506e269c617807
Sara Cappelletti <sara.cappelletti4@studio.unibo.it>
2023-01-02 10:56:32 +0100

    Added modal file

--- profile.php
+++ profile.php
@@ -2,7 +2,7 @@
 require_once 'bootstrap.php';
 
 $templateParams["title"] = "Nome sito - Profilo";
-$templateParams["js"] = array("https://unpkg.com/axios/dist/axios.min.js", "js/profile_header.js", "js/profile.js");
+$templateParams["js"] = array("https://unpkg.com/axios/dist/axios.min.js", "js/profile_header.js", "js/profile.js", "js/modal.js");
 
 require 'template/base.php'
 ?>
