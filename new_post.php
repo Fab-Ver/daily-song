77bd5141b911951b5a02b1a5bf31756c09163232
Fabio Veroli <fabio.veroli@studio.unibo.it>
2023-01-03 17:48:59 +0100

    Refactor names

--- new_post.php
+++ new_post.php
@@ -3,16 +3,16 @@ require_once 'bootstrap.php';
 secure_session_start();
 
 if(isUserLoggedIn()){
-    $posts = $dbh->getUserPosts($_SESSION["username"]);
+    $posts = $dbh->getUserPosts($_SESSION['username']);
     $alreadyPost = false;
     foreach($posts as $post){
-        if(hours_date_diff(date("Y-m-d H:i:s"),$post["dateTime"]) < 24){
+        if(hours_date_diff(date("Y-m-d H:i:s"),$post['dateTime']) < 24){
             $alreadyPost = true;
         }
     }
     if($alreadyPost){
-        $templateParams["title"] = "Nome sito - New Post";
-        $templateParams["error"] = '
+        $templateParams['title'] = "Nome sito - New Post";
+        $templateParams['error'] = '
             <article>
                 <h2>You already posted</h2>
                 <p>Come back here tomorrow to post your new song of the day</p>
@@ -20,7 +20,7 @@ if(isUserLoggedIn()){
         ';
         require 'template/base.php';
     } else {
-        $templateParams["title"] = "Nome sito - New Post";
+        $templateParams['title'] = 'Nome sito - New Post';
         $templateParams["js"] = array("js/config.js","https://unpkg.com/axios/dist/axios.min.js","utils/functions.js","js/new_post.js");
     
         require 'template/base.php';
