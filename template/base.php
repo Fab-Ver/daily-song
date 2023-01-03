5987c51ae0bfbb2950854d1ecf26ca99fff863c3
Sara Cappelletti <sara.cappelletti4@studio.unibo.it>
2023-01-03 15:49:32 +0100

    Added default_style css

--- template/base.php
+++ template/base.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8" />
     <title><?php echo $templateParams["title"]; ?></title>
     <link rel="stylesheet" type="text/css" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css" />
+    <link rel="stylesheet" type="text/css" href="./css/default_style.css" />
     <link rel="stylesheet" type="text/css" href="./css/style.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="decription" content="is a digital music social that gives you the possibility to share a day song with your friends"/>
