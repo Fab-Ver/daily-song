8d19f07c2add7b1b71eabc4a9b1bee0dba38a209
Fabio Veroli <fabio.veroli@studio.unibo.it>
2023-01-04 20:51:30 +0100

    Change error messages and response structure

--- js/new_post.js
+++ js/new_post.js
@@ -1,11 +1,10 @@
 const regex = /^(?:spotify:|(?:https?:\/\/(?:open|play)\.spotify\.com\/))(?:embed)?\/?(track)(?::|\/)((?:[0-9a-zA-Z]){22})/;
-const main = document.querySelector("main");
-main.classList.add("container");
+const main = document.querySelector('main');
 main.innerHTML = createNewPostForm();
 const url_box = document.getElementById('url_track');
 const description = document.getElementById('description');
 const comments = document.getElementById('comments');
-axios.get("genre.php").then(response => {
+axios.get('genre.php').then(response => {
     let dropdown = document.getElementById('genres_list');
     dropdown.innerHTML += createGenres(response.data);
 });
@@ -54,8 +53,6 @@ function checkURL(){
         let url = url_box.value;
         let match = url.match(regex);
         if(match){
-            //console.log(match[1]);
-            //console.log(match[2]);
             setValid(url_box,true);
         } else {
             showError(url_box,"Wrong URL format, expected:\n - https:\/\/play.spotify.com\/track\/id\n - https:\/\/open.spotify.com\/track\/id\n - spotify:track:id");
@@ -71,12 +68,12 @@ function checkNewPostForm(){
     let err_element = new Array();
 
     if(url_box.validity.valueMissing || url_box.getAttribute('aria-invalid') === 'true'){
-        errors.push("Invalid or missing Spotify URL.");
+        errors.push("Spotify track URL required, enter a valid URL to continue");
         err_element.push(url_box);
     }
 
     if(!checkGenres(0,3)){
-        errors.push("Wrong number of track genres, too low or too high");
+        errors.push("Wrong number of selected genresID, please select between 1 and 3 genres");
     }
 
     if(errors.length == 0){
@@ -86,7 +83,7 @@ function checkNewPostForm(){
             setValid(element,false);
         });
         let error_div = document.querySelector("div.error_form");
-        error_div.innerHTML = `While trying to add a new post the following errors occurred:<ul>` + createError(errors);
+        error_div.innerHTML = `While processing your data the following errors occurred:<ul>` + createError(errors);
         error_div.removeAttribute('hidden');
         error_div.focus();
     }
@@ -96,45 +93,46 @@ function submitNewPostForm(){
     let formID = new FormData();
     let track_id = url_box.value.match(regex)[2];
     formID.append('checkTrackID',track_id);
-    
     axios.post('api-track.php',formID).then(response => {
-        if(response.data.loggedIn){
-            if(!response.data.checkTrackID){
+        let error_div = document.querySelector('div.error_form');
+        if(response.data.errorMsg !== ""){
+            error_div.innerHTML = response.data.errorMsg;
+            error_div.removeAttribute('hidden');
+            error_div.focus();
+        } else if(response.data.checkTrackID === false){
                 retrieveData(track_id).then(track_data => {
-                    let formNewTrack = new FormData();
-                    let artists = track_data.artists;
-                    let artists_names = new Array();
-                    artists.forEach(a => {
-                        artists_names.push(a.name);
-                    });
-                    formNewTrack.append('trackID', track_id);
-                    formNewTrack.append('urlSpotify',track_data.external_urls.spotify);
-                    formNewTrack.append('urlImage', track_data.album.images[1].url);
-                    formNewTrack.append('urlPreview',track_data.preview_url);
-                    formNewTrack.append('title',track_data.name);
-                    formNewTrack.append('artist',artists_names.toString());
-                    formNewTrack.append('albumName',track_data.album.name);
-                    axios.post('api-track.php',formNewTrack).then(new_track_response => {
-                        if(new_track_response.data.loggedIn){
-                            if(new_track_response.data.insertTrack){
-                                createPost(track_id);
-                            } else {
-                                let error_div = document.querySelector("div.error_form");
-                                error_div.innerHTML = `Undefined error while retrieving track data, try later.`;
+                    if(track_data.error_string !== undefined){
+                        error_div.innerHTML = 'SPOTIFY: ' + track_data.error_string.toUpperCase() + ', try later.';
+                        error_div.removeAttribute('hidden');
+                        error_div.focus();
+                    } else {
+                        let formNewTrack = new FormData();
+                        let artists = track_data.artists;
+                        let artists_names = new Array();
+                        artists.forEach(a => {
+                         artists_names.push(a.name);
+                        });
+                        formNewTrack.append('trackID', track_id);
+                        formNewTrack.append('urlSpotify',track_data.external_urls.spotify);
+                        formNewTrack.append('urlImage', track_data.album.images[1].url);
+                        formNewTrack.append('urlPreview',track_data.preview_url);
+                        formNewTrack.append('title',track_data.name);
+                        formNewTrack.append('artist',artists_names.toString());
+                        formNewTrack.append('albumName',track_data.album.name);
+                        axios.post('api-track.php',formNewTrack).then(new_track_response => {
+                            if(new_track_response.data.errorMsg !== ""){
+                                error_div.innerHTML = new_track_response.data.errorMsg;
                                 error_div.removeAttribute('hidden');
                                 error_div.focus();
+                            } else if(new_track_response.data.insertTrack === true){
+                                createPost(track_id);
                             }
-                        } else {
-                            window.location.replace("index.php");
-                        }
-                    });
-                })
-            } else {
+                        });
+                    }
+                });
+        } else if(response.data.checkTrackID === true){
                 createPost(track_id);
-            }
-        } else {
-            window.location.replace("index.php");
-        } 
+        }
     });
 }
 
@@ -144,18 +142,14 @@ function createPost(track_id){
     formPost.append('activeComments',comments.checked.toString());
     formPost.append('trackID',track_id);
     formPost.append('genresID',JSON.stringify(getGenresID()));
-    formPost.append('dateTime',new Date().toISOString().slice(0, 19).replace('T', ' '));
     axios.post('api-new-post.php',formPost).then(response => {
-        if(response.data.loggedIn){
-            if(!response.data.errorNewPost){
-                window.location.replace("homepage.php");
-            } else {
-                error_div.innerHTML = `Undefined error while creating a new post, try later.`;
-                error_div.removeAttribute('hidden');
-                error_div.focus();
-            }
-        } else {
-             window.location.replace("index.php");
+        let error_div = document.querySelector('div.error_form');
+        if(response.data.errorMsg !== ""){
+            error_div.innerHTML = response.data.errorMsg;
+            error_div.removeAttribute('hidden');
+            error_div.focus();
+        } else if(response.data.postInserted === true){
+            window.location.replace("homepage.php");
         }
     });
 }
