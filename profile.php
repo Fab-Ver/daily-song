59094635eebef98ae53072807792a1c91b7b1884
Sara Cappelletti <sara.cappelletti4@studio.unibo.it>
2023-01-05 16:06:46 +0100

    removed selector

--- profile.php
+++ profile.php
@@ -2,7 +2,7 @@
 require_once 'bootstrap.php';
 
 $templateParams["title"] = "Nome sito - Profile";
-$templateParams["js"] = array("https://unpkg.com/axios/dist/axios.min.js","js/like.js", "js/profile_header.js",  "js/profile.js", "js/modal.js", "js/selector.js");
+$templateParams["js"] = array("https://unpkg.com/axios/dist/axios.min.js", "js/like.js", "js/profile_header.js",  "js/profile.js", "js/modal.js");
 
 require 'template/base.php'
 ?>
