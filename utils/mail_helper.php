ef4e0d0e026de7e98b029bf33a7972ce98aadf70
Fabio Veroli <fabio.veroli@studio.unibo.it>
2023-01-09 17:54:31 +0100

    Add function to send blocked user mail and notification mail

--- utils/mail_helper.php
+++ utils/mail_helper.php
@@ -42,5 +42,37 @@ require 'vendor/autoload.php';
                 return false;
             }
         }
+
+        public function sendEmailNotification(string $recipient, string $message, string $subject) : bool{
+            $this->mail->Subject = $subject;
+            $this->mail->addAddress($recipient);
+            $this->mail->Body = $message;
+            try{
+                if($this->mail->send()){
+                    return true;
+                }
+            } catch (Exception $e){
+                return false;
+            }
+        }
+
+        public function sendBlockedAccountEmail(string $recipient){
+            $this->mail->Subject = 'Blocked Account';
+            $body='<p>Your account has been blocked due to too many failed login attempts.</p>';
+            $body.='<p>-------------------------------------------------------------</p>';
+            $body.='<p>Was it you? Then try to <a href="http://localhost/progetto_web/forgot_password.php">reset the password</a>.</p>';	
+            $body.='<p>Wasn\'t it you? Make sure you reset your password as soon as your account is unlocked (about 3 hours)</p>';	
+            $body.='<p>-------------------------------------------------------------</p>';  	
+            $body.='<p>Progetto Web</p>';
+            $this->mail->Body = $body;
+            $this->mail->addAddress($recipient);
+            try{
+                if($this->mail->send()){
+                    return true;
+                }
+            } catch (Exception $e) {
+                return false;
+            }
+        }
     }
 ?>
