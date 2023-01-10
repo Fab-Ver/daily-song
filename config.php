8f11c4d2384fb3bc021ebbe6d61a33d678d71c20
Fabio Veroli <fabio.veroli@studio.unibo.it>
2023-01-10 14:49:26 +0100

    Add empty config file

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+define('USERNAME',null); /**Insert here your gmail email */
+define('PASSWORD',null); /**Insert here you gmail application password, see README yto know how to request it. */
+?>
